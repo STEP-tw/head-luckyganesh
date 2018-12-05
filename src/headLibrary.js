f2f21e4805d9a370c610b9b23cb47a44f6bf4388
Saiganeb <saiganeb@in-Saiganeb.local>
2018-12-05 18:17:14 +0530

    extrated functions from head function

--- src/headLibrary.js
+++ src/headLibrary.js
@@ -33,25 +33,42 @@ const checkingErrors = function(parsedInputs){
   return "";
 }
 
-const head = function(parsedInputs,reader,fileChecker){
-  let { options ,length , files } = parsedInputs;
-  files = files.map(fileStructure);
+getContentOfFile = function(files,reader,existChecker,fileChecker){
   files = files.map((file) => {
+    if(!isExists(existChecker,file.fileName)){
+      return ("head: "+file.fileName+": No such file or directory")
+    }
     file.contents = read(reader,"utf-8",file.fileName);
     return file;
   });
+  return files;
+};
+
+const extractContent = function( files,options,length){
   contents = files.map((file) => {
     if(options == 'n'){
       return file.getLines(length);
     }
     return file.getBytes(length);
   });
+  return contents;
+};
+
+const showFormat = function(contents,files){
   if(contents.length == 1){
     return contents[0];
   }
   return contents.map((content,index) => {
     return createHeading(files[index].fileName)+"\n"+content+"\n";
   }).join('\n');;
+}
+
+const head = function(parsedInputs,reader,existChecker,fileChecker){
+  let { options ,length , files } = parsedInputs;
+  files = files.map(fileStructure);
+  files = getContentOfFile(files,reader,existChecker,fileChecker);
+  let contents = extractContent(files,options,length,existChecker);
+  return showFormat(contents,files);
 }
 
 module.exports = { parseInputsOfHead , read , head ,checkingErrors, isExists , checkingErrors};
