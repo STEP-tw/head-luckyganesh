2c2ef60cf28e9ad198ab5bcf5d04c934ebb85b57
Saiganeb <saiganeb@in-Saiganeb.local>
2018-12-05 11:58:13 +0530

    implemented parseInputsOfHead function

--- src/headLibrary.js
+++ src/headLibrary.js
@@ -5,13 +5,29 @@ const applyFunction = function(funcName,argumentToPass){
   return funcName(argumentToPass);
 }
 
+const findOption = function(elem){
+  if(elem[0] != "-" || +elem.slice(1)){
+    return 'n';
+  }
+  return elem[1];
+};
+const findLength = function(options){
+  let length = "10";
+  if(options[0] == "-"){
+    length = +options[0].slice(1) || +options[0].slice(2) || options[1];
+  }
+  return length;
+}
+
 const isFileName = function(fileName){
   return fileName[0] != "-" && !(+fileName);
 }
 
 const parseInputsOfHead = function(headInputs){
+  let options = findOption(headInputs[0]);
+  let length = findLength(headInputs.slice(0,2));
   let files = headInputs.filter(isFileName);
-  return { options:'n', length:10 , files };
+  return { options, length , files };
 }
 
 module.exports = { applyFunction ,parseInputsOfHead};
--- test/headLibraryTest.js
+++ test/headLibraryTest.js
@@ -22,5 +22,11 @@ describe('parseInputsOfHead',function(){
   it('should return all inputs as files',function(){
     deepEqual(parseInputsOfHead(["file1","file2"]),{ options:"n" , length:10 , files:["file1","file2"]});
   });
+  it('should return the option and length with files',function(){
+    deepEqual(parseInputsOfHead(["-n","10","file1","file2"]),{ options:"n" , length:10 , files:["file1","file2"]});
+  });
+  it('should return the option other than n',function(){
+    deepEqual(parseInputsOfHead(["-c","10","file1","file2"]),{ options:"c" , length:10 , files:["file1","file2"]});
+  });
 });
 
