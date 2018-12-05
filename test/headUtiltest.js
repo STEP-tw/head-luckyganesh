4ebf66946527c4d731a34d6e58074094d7aa81ed
Saiganeb <saiganeb@in-Saiganeb.local>
2018-12-05 13:01:09 +0530

    write test cases for findOption function

--- test/headUtiltest.js
+++ test/headUtiltest.js
@@ -1,6 +1,8 @@
 const { deepEqual } = require('assert');
 
-const { getContent,createHeading } = require('../src/headUtil.js');
+const { getContent,createHeading,
+findOption
+} = require('../src/headUtil.js');
 
 describe('getContent',() => {
   let file = { contents:"hello,hi,bye,good,bad"};
@@ -25,5 +27,18 @@ describe('createHeading',function(){
   });
   it('should create heading for numbers',function(){
     deepEqual(createHeading(123),"==> 123 <==");
+  });
+});
+
+describe('findOption',function(){
+  it('should return default option n for name of file',function(){
+    deepEqual(findOption("file"),"n");
+  });
+  it('should return exact option they given',function(){
+    deepEqual(findOption('-n'),'n');
+    deepEqual(findOption('-c'),'c');
+  });
+  it('should return default case for number',function(){
+    deepEqual(findOption('-5'),'n');
   });
 });
