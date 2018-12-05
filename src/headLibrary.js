a396146b6194e18fee844594215ab794e2e5dd7f
Saiganeb <saiganeb@in-Saiganeb.local>
2018-12-05 11:06:10 +0530

    refactored the parseInputsOfHead function

--- src/headLibrary.js
+++ src/headLibrary.js
@@ -5,8 +5,13 @@ const applyFunction = function(funcName,argumentToPass){
   return funcName(argumentToPass);
 }
 
+const isFileName = function(fileName){
+  return fileName[0] != "-" && !(+fileName);
+}
+
 const parseInputsOfHead = function(headInputs){
-  return { options:'n', length:10 , files:headInputs };
+  let files = headInputs.filter(isFileName);
+  return { options:'n', length:10 , files };
 }
 
 module.exports = { applyFunction ,parseInputsOfHead};
