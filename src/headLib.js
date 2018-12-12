3a68028b443b4bbb11094a7d511ce728d9283bd4
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-12 10:11:17 +0530

    added tail.js file to work as tail.

--- src/headLib.js
+++ src/headLib.js
@@ -1,17 +1,20 @@
-const {fileStructure} = require('./fileLibrary');
+const { fileStructure } = require("./fileLibrary");
 
-const { getContentOfFiles , parseInputs ,checkingErrors } = require('./lib.js');
+const { getContentOfFiles, parseInputs, checkingErrors } = require("./lib.js");
 
-const head = function(parsedInputs, fs,type) {
-    let { options, length, files } = parsedInputs;
-    files = files.map(fileStructure.bind(null,type));
-    files = getContentOfFiles(files , options, length, fs,type);
-    return files.map(file => file.contents).join("\n");
+const head = function(parsedInputs, fs, type) {
+  let { options, length, files } = parsedInputs;
+  if(type == "tail"){
+      length = +length+1;
+  }
+  files = files.map(fileStructure.bind(null, type));
+  files = getContentOfFiles(files, options, length, fs, type);
+  return files.map(file => file.contents).join("\n");
 };
 
-const runHead = function(userInputs,fs,type){
-    let parsedInputs = parseInputs(userInputs);
-    let err = checkingErrors(parsedInputs);
-    return err || head(parsedInputs, fs,type)
-}
-module.exports = { head , runHead };
+const runHead = function(userInputs, fs, type) {
+  let parsedInputs = parseInputs(userInputs);
+  let err = checkingErrors(parsedInputs);
+  return err || head(parsedInputs, fs, type);
+};
+module.exports = { head, runHead };
--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
-
-
-
+const { runHead } = require('./src/headLib.js')
+const fs = require("fs");
+const main = function() {
+  console.log(runHead(process.argv.slice(2),fs,"tail"))
+};
+main();
