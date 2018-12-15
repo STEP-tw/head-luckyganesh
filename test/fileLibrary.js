af51f3d805c336add8ed0dbee7011fb83bc642dc
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-15 08:11:50 +0530

    added tests for fileStructure function

--- test/fileLibrary.js
+++ test/fileLibrary.js
@@ -2,8 +2,23 @@ const { deepEqual } = require("assert");
 
 const { fileStructure } = require("../src/fileLibrary.js");
 
+const { getContent , getContentOfTail} = require('../src/util.js')
+
 describe("fileStructure", () => {
-  it("should create an empty file contents", () => {
-    deepEqual(fileStructure("head","file").contents, "");
+  it("should create an empty file structure for  head", () => {
+    let expected = {fileName : "file" , contents : ""};
+    expected.getLines = getContent.bind(expected,"\n");
+    expected.getBytes = getContent.bind(expected,"");
+    actual = fileStructure("head","file");
+    deepEqual(actual.fileName,expected.fileName);
+    deepEqual(actual.contents,expected.contents);
   });
+  it('should create an empty file structure for tail', () => {
+    let expected = {fileName : "file" , contents : ""};
+    expected.getLines = getContentOfTail.bind(expected,"\n");
+    expected.getBytes = getContentOfTail.bind(expected,"");
+    actual = fileStructure("tail","file");
+    deepEqual(actual.fileName,expected.fileName);
+    deepEqual(actual.contents,expected.contents);
+  })
 });
