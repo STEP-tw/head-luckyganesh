baf3fcbdf5a9d2229dd43b48bfdcd1bc38e3565c
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-17 16:00:18 +0530

    Deleted repeated code in util.

--- src/util.js
+++ src/util.js
@@ -20,48 +20,8 @@ const createHeading = function(name) {
   return heading;
 };
 
-const isNumber = function(number){
-  return +number == number || number === 0 ;
-};
-
-const findOption = function(elem) {
-  if (elem[0] != "-" || isNumber(elem.slice(1))) {
-    return "n";
-  }
-  return elem[1];
-};
-
-const isDefined = function(variable){
-  return variable !== "" && variable != undefined ;
-};
-
-const findLength = function(userArgs) {
-  let firstArg = userArgs[0];
-  if (!firstArg.startsWith("-")) {
-    return 10;
-  }
-  firstArg = firstArg.slice(1);
-  if (isNumber(firstArg)) {
-    return firstArg;
-  }
-  firstArg = firstArg.slice(1);
-  if (isDefined(firstArg)) {
-    return firstArg;
-  }
-  return userArgs[1];
-};
-
-const isFilePath = function(filePath) {
-  return filePath[0] != "-" && !isNumber(filePath);
-};
-
 module.exports = {
   getContentFromTop,
   getContentFromBottom,
-  createHeading,
-  findOption,
-  findLength,
-  isFilePath,
-  isNumber,
-  isDefined
+  createHeading
 };
--- test/utilTest.js
+++ test/utilTest.js
@@ -4,11 +4,6 @@ const {
   getContentFromTop,
   getContentFromBottom,
   createHeading,
-  findOption,
-  findLength,
-  isFilePath,
-  isNumber,
-  isDefined
 } = require("../src/util.js");
 
 describe("getContentFromTop", () => {
@@ -52,76 +47,5 @@ describe("createHeading", function() {
   });
   it("should create heading for numbers", function() {
     deepEqual(createHeading(123), "==> 123 <==");
-  });
-});
-
-describe("findOption", function() {
-  it("should return default option n for name of file", function() {
-    deepEqual(findOption("file"), "n");
-  });
-  it("should return exact option they given", function() {
-    deepEqual(findOption("-n"), "n");
-    deepEqual(findOption("-c"), "c");
-  });
-  it("should return default case for number", function() {
-    deepEqual(findOption("-5"), "n");
-  });
-});
-
-describe("findLength", function() {
-  it("should return default length n for name of file", function() {
-    deepEqual(findLength(["file", "file2"]), 10);
-  });
-  it("should return exact length they given", function() {
-    deepEqual(findLength(["-n", "8"]), 8);
-    deepEqual(findLength(["-c", "3"]), 3);
-  });
-  it("should return length even if it is included in option", function() {
-    deepEqual(findLength(["-n5", "79"]), 5);
-    deepEqual(findLength(["-c56", "file"]), 56);
-  });
-  it("should return default case for number", function() {
-    deepEqual(findLength(["-5", "file"]), 5);
-  });
-});
-
-describe("isFilePath", function() {
-  it("should tell the truth for name", function() {
-    deepEqual(isFilePath("file"), true);
-  });
-  it("should tell the false for not a name", function() {
-    deepEqual(isFilePath("5"), false);
-    deepEqual(isFilePath("-n5"), false);
-    deepEqual(isFilePath("-5"), false);
-  });
-});
-
-describe('isNumber', function() {
-  it('should return true for 0', function() {
-    deepEqual(isNumber(0),true);
-  });
-  it('should return true for any positve number', function() {
-    deepEqual(isNumber(1),true);
-    deepEqual(isNumber(10),true);
-  });
-  it('should return true for negative numbers',() => {
-    deepEqual(isNumber(-1),true);
-  });
-  it('should return true for numbers even if the numbers in strings', () => {
-    deepEqual(isNumber('1'),true);
-    deepEqual(isNumber('0'),true);
-    deepEqual(isNumber('-1'),true);
-  });
-});
-
-describe('isDefined', function() {
-  it('should return true for defined variable', function() {
-    deepEqual(isDefined(0),true);
-    deepEqual(isDefined("msg"),true);
-    deepEqual(isDefined([1,2,3]),true);
-  });
-  it('should return false for undefined variable', function() {
-    deepEqual(isDefined(),false);
-    deepEqual(isDefined(""),false);
   });
 });
