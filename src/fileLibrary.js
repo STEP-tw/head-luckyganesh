9c94e9795230fc2e2eddb4ed589edb8e1adf8233
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-18 15:03:47 +0530

    Refactored fileStructure function.

--- src/fileLibrary.js
+++ src/fileLibrary.js
@@ -2,10 +2,12 @@ const { getContentFromTop , getContentFromBottom } = require("./util.js");
 
 const fileStructure = function(coreUtil,fileName) {
   const commandTypes = { head :getContentFromTop , tail:getContentFromBottom };
-  let file = { fileName, contents: "" };
-  file.getLines = commandTypes[coreUtil].bind(null, "\n");
-  file.getBytes = commandTypes[coreUtil].bind(null, "");
-  return file;
+  return {
+    fileName,
+    contents:"",
+    getLines:commandTypes[coreUtil].bind(null, "\n"),
+    getBytes:commandTypes[coreUtil].bind(null, "")
+  };
 };
 
 module.exports = { fileStructure };
