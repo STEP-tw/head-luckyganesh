43d7312451de18006706d8e4201ea7bf845bb4ae
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-18 10:06:29 +0530

    Removed unnecessary bindings.

--- src/fileLibrary.js
+++ src/fileLibrary.js
@@ -3,8 +3,8 @@ const { getContentFromTop , getContentFromBottom } = require("./util.js");
 const fileStructure = function(commandType,fileName) {
   commandTypes = { head :getContentFromTop , tail:getContentFromBottom };
   let file = { fileName, contents: "" };
-  file.getLines = commandTypes[commandType].bind(file, "\n");
-  file.getBytes = commandTypes[commandType].bind(file, "");
+  file.getLines = commandTypes[commandType].bind(null, "\n");
+  file.getBytes = commandTypes[commandType].bind(null, "");
   return file;
 };
 
