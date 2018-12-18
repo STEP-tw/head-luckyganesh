cc6efe3e8a92ea8368ac1c0a59b16f144242c4fe
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-18 09:15:34 +0530

    Added echo's in run_all_test.sh file.

--- scripts/run_all_test.sh
+++ scripts/run_all_test.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 set -e
+echo "------- head tests are running -------"
 ./scripts/run_test.sh head.js ./app_test_data/headInput ./app_test_data/headOutput 
+echo "------- head tests are passed -------"
+echo "------- tail tests are running -------"
 ./scripts/run_test.sh tail.js ./app_test_data/tailInput ./app_test_data/tailOutput 
+echo "------- tail tests are passed -------"
 mocha --reporter min
+if [ $? -eq 0 ];then
+    echo "------ all tests passed -------"
+    fi
